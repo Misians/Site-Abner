b1441b397b335fd96750c0bd53061fb557920c26
Misians <artemisiakimberlly@alu.uern.br>
2024-07-02 22:07:28 -0300

    att

--- src/components/Academico.tsx
+++ src/components/Academico.tsx
@@ -2,6 +2,73 @@ import React from 'react'
 
 export const Academico = () => {
   return (
-    <div>Academico</div>
+    <div className='info-acad'>
+      <h3>Formação acadêmica/titulação</h3>
+      <div className="formacao">
+        <div className="ano"><h5>2008 - 2012</h5></div>
+        
+        <p>
+        Doutorado em Engenharia Elétrica.
+        Universidade Federal do Rio Grande do Norte, UFRN, Brasil.
+        Título: Mapeamento Robótico 2,5-D com Representação em Grade de Ocupação-Elevação , Ano de obtenção: 2012.
+        Orientador: Luiz Marcos Garcia Gonçalves.
+        Bolsista do(a): Conselho Nacional de Desenvolvimento Científico e Tecnológico, CNPq, Brasil.
+        Grande área: Outros
+        </p>
+      </div>
+      <div className="formacao">
+        <div className="ano"><h5>2002 - 2008</h5></div>
+        
+        <p>
+          Mestrado em Engenharia Elétrica.
+          Universidade Federal do Rio Grande do Norte, UFRN, Brasil.
+          Título: Mapeamento com Sonar usando grade de ocupação baseado em modelagem probabilística , Ano de Obtenção: 2008.
+          Orientador: Luiz Marcos Garcia Gonçalves.
+          Bolsista do(a): Coordenação de Aperfeiçoamento de Pessoal de Nível Superior, CAPES, Brasil.
+          Palavras-chave: Odometria; Mapeamento; Grade de Ocupação; probabilidade.
+          Grande área: Outros
+        </p>
+      </div>
+      <div className="formacao">
+        <div className="ano"><h5>2008 - 2012</h5></div>
+        
+        <p>
+        Doutorado em Engenharia Elétrica.
+        Universidade Federal do Rio Grande do Norte, UFRN, Brasil.
+        Título: Mapeamento Robótico 2,5-D com Representação em Grade de Ocupação-Elevação , Ano de obtenção: 2012.
+        Orientador: Luiz Marcos Garcia Gonçalves.
+        Bolsista do(a): Conselho Nacional de Desenvolvimento Científico e Tecnológico, CNPq, Brasil.
+        Grande área: Outros
+        </p>
+      </div>
+      <div className="formacao">
+        <div className="ano"><h5>2001 - 2005</h5></div>
+        
+        <p>Graduação em Engenharia de Computação.
+          Universidade Federal do Rio Grande do Norte, UFRN, Brasil.
+          Título: Construção e Teleoperação de uma Plataforma Robótica Móvel.
+          Orientador: Luiz Marcos Garcia Gonçalves.
+          Bolsista do(a): Conselho Nacional de Desenvolvimento Científico e Tecnológico, CNPq, Brasil.
+        </p>
+      </div>
+      <div className="formacao">
+        <div className="ano">
+          <h5>1996-2000</h5>
+        </div>
+        
+        <p>Curso técnico/profissionalizante em Eletromecânica.
+        Centro Federal de Educação Tecnológica  do Rio Grande do Norte, CEFET-RN, Brasil.
+        </p>
+      </div>
+      <div className="formacao">
+        <div className="ano">
+          <h5>1996-2000</h5>
+        </div>
+        
+        <p>Ensino Médio (2º grau).
+        Centro Federal de Educação Tecnológica do Rio Grande do Norte, CEFET-RN, Brasil.
+        </p>
+      </div>
+    </div>
   )
 }
